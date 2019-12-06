ca8dfaa0ed09487fefe330e73ba0cb8bcbdf4808
hamdyaea <hamdy.aea@protonmail.com>
2019-12-06 21:49:00 +0100

    Code beauty

--- DiceSimulator.py
+++ DiceSimulator.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 # developer : Hamdy Abou El Anein
+# hamdy.aea@protonmail.com
 
 from easygui import *
 import random
 import sys
-
 
 
 dice1 = "./images/dice1.gif"
@@ -19,42 +21,46 @@ def dice():
     outputNum = list(range(inputNum + 1))
     outputNum.remove(0)
 
-
     numbers = random.shuffle(outputNum)
-
 
     top6 = outputNum[:1]
 
     if top6 == [1]:
-        image=dice1
+        image = dice1
     elif top6 == [2]:
-        image=dice2
+        image = dice2
     elif top6 == [3]:
-        image=dice3
+        image = dice3
     elif top6 == [4]:
-        image=dice4
+        image = dice4
     elif top6 == [5]:
-        image=dice5
+        image = dice5
     else:
-        image=dice6
+        image = dice6
 
-
-
-    diceChoice = ["Replay","Quit"]
-
-    replay = buttonbox(image=image,choices=diceChoice,title="Dice simulator", msg="Dice simualtor\n\nNumber on the dice : " + str(top6))
+    diceChoice = ["Replay", "Quit"]
+    top6 = str(top6)
+    top6 = top6.replace("[", "")
+    top6 = top6.replace("]", "")
+    replay = buttonbox(
+        image=image,
+        choices=diceChoice,
+        title="Dice simulator",
+        msg="Dice simualtor\n\nNumber on the dice : " + str(top6),
+    )
     if replay == "Replay":
         dice()
-    else :
+    else:
         sys.exit(0)
+
 
 logoDice = "./images/dice-full.gif"
 msg = "Do you want to play ?"
-choices = ["Yes","No"]
+choices = ["Yes", "No"]
 
-begining = buttonbox(msg,image=logoDice,choices=choices)
+begining = buttonbox(msg, image=logoDice, choices=choices)
 
 if begining == "Yes":
     dice()
-else :
+else:
     sys.exit(0)
